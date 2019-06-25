9ea87de1ff12f83fc53f5f5f07bed8efc5315aa4
FriedRice <n.mclaughlin21@gmail.com>
2019-06-24 23:26:35 -0400

    Made a base cron docker image to use for all containers running cron jobs

--- docker/setup_scripts/cron_entrypoint.sh
+++ docker/setup_scripts/cron_entrypoint.sh
@@ -1,6 +1,35 @@
 #!/bin/bash
 
-# Write all env vars set by docker into an env file for cron to use
+set -e
+
+# Write all env vars set by docker into an env file for cron to use.
 declare -p | grep '^declare -x' > $ENV_FILE
+echo "Setting cron environment to:"
+cat $ENV_FILE
+echo
+
+# Swap in cron job schedules from env vars.
+cron_schedule_var_names="$(env | cut -d "=" -f 1 | grep ".*CRON_SCHEDULE")"
+if [ ! -n "$cron_schedule_var_names" ]; then
+    echo "No environment variables ending with \"CRON_SCHEDULE\" found"
+    exit 1
+fi
+
+for cron_schedule_var_name in $cron_schedule_var_names; do
+    cron_schedule=${!cron_schedule_var_name}
+
+    # Escape strings for use in a sed find replace
+    cron_schedule_var_name_escaped="$(sed 's/[^^]/[&]/g' <<< \
+        "$cron_schedule_var_name")"
+    cron_schedule_escaped="$(sed 's/[\/&]/\\&/g' <<< "$cron_schedule")"
+
+    sed -i -e "s/$cron_schedule_var_name_escaped/$cron_schedule_escaped/g" \
+        $CRON_FILE
+    echo -n "Successfully swaped \"$cron_schedule_var_name\" for "
+    echo "\"$cron_schedule\" in $CRON_FILE"
+done
+
+# Cron file must end with an empty line to be valid.
+echo >> $CRON_FILE
 
 cron -f
