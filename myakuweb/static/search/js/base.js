9d7e571b53b51afc9ac20a64382038f669df8615
FriedRice <n.mclaughlin21@gmail.com>
2019-08-29 14:31:58 -0400

    Made search box placeholder text adjust based on viewport size

--- myakuweb/static/search/js/base.js
+++ myakuweb/static/search/js/base.js
@@ -1,17 +1,34 @@
+// Myaku javascript
+
+const FULL_SEARCH_PLACEHOLDER = 'Japanese word, set phrase, idiom, etc.';
+const SHORT_SEARCH_PLACEHOLDER = 'Japanese word, phrase, etc.';
+const MOBILE_VIEWPORT_WIDTH_MAX = 768;  // in pixels.
+
+function updateSearchPlaceholder() {
+    if (window.innerWidth <= MOBILE_VIEWPORT_WIDTH_MAX) {
+        $('#search-input').attr('placeholder', SHORT_SEARCH_PLACEHOLDER);
+    } else {
+        $('#search-input').attr('placeholder', FULL_SEARCH_PLACEHOLDER);
+    }
+}
+
+updateSearchPlaceholder();
+$(window).resize(updateSearchPlaceholder);
+
 $('.search-clear').click(function () {
     $(this).closest('.search-form').find('#query').val('');
-})
+});
 
 $('.extra-sample-instances').on('show.bs.collapse', function () {
     var trigger_button = $(this)
         .closest('.result-tile')
         .find('.show-more-button');
     trigger_button.text(trigger_button.text().replace('more', 'less'));
-})
+});
 
 $('.extra-sample-instances').on('hide.bs.collapse', function () {
     var trigger_button = $(this)
         .closest('.result-tile')
         .find('.show-more-button');
     trigger_button.text(trigger_button.text().replace('less', 'more'));
-})
+});
