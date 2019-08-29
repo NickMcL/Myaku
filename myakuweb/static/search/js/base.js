8b1348870265fa2bc6919d034aca490943fbe8b0
FriedRice <n.mclaughlin21@gmail.com>
2019-08-28 22:50:46 -0400

    Added clear button for search box

--- myakuweb/static/search/js/base.js
+++ myakuweb/static/search/js/base.js
@@ -1,11 +1,15 @@
-$('.extra-sample-instances').on('show.bs.collapse', function() {
+$('.search-clear').click(function () {
+    $(this).closest('.search-form').find('#query').val('');
+})
+
+$('.extra-sample-instances').on('show.bs.collapse', function () {
     var trigger_button = $(this)
         .closest('.result-tile')
         .find('.show-more-button');
     trigger_button.text(trigger_button.text().replace('more', 'less'));
 })
 
-$('.extra-sample-instances').on('hide.bs.collapse', function() {
+$('.extra-sample-instances').on('hide.bs.collapse', function () {
     var trigger_button = $(this)
         .closest('.result-tile')
         .find('.show-more-button');
