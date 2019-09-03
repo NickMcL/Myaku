2c05b5c9fdbea6e4fd17d1b8ce6cea4af5630e8c
FriedRice <n.mclaughlin21@gmail.com>
2019-09-03 19:25:12 -0400

    Fixed some areas that missed renaming

--- docker/myaku_redis.first-page-cache/start_redis.sh
+++ docker/myaku_redis.first-page-cache/start_redis.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 exec redis-server $REDIS_CONF_FILE \
-    --requirepass "$(cat $MYAKU_SEARCH_RESULT_CACHE_PASSWORD_FILE)" \
+    --requirepass "$(cat $MYAKU_FIRST_PAGE_CACHE_PASSWORD_FILE)" \
     --dir "$REDIS_DATA_DIR"
