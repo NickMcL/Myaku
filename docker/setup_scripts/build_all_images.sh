b713ac35cf1374aa3ae7afa101abd76a06012c4a
FriedRice <n.mclaughlin21@gmail.com>
2019-09-10 09:35:03 -0400

    Added image pull to start of build_all_images.sh

--- docker/setup_scripts/build_all_images.sh
+++ docker/setup_scripts/build_all_images.sh
@@ -5,6 +5,16 @@ set -ev
 
 cd "$(dirname "${BASH_SOURCE[0]}")"/../..
 
+# Pull the latest prod images first so that their layers can be used if
+# possible instead of rebuilding all of the layers for all images.
+sudo docker pull friedrice2/myaku_crawler:latest
+sudo docker pull friedrice2/myaku_rescore:latest
+sudo docker pull friedrice2/myaku_web:latest
+sudo docker pull friedrice2/myaku_nginx.reverseproxy:latest
+sudo docker pull friedrice2/myaku_redis.first-page-cache:latest
+sudo docker pull friedrice2/myaku_mongo.crawldb:latest
+sudo docker pull friedrice2/mongobackup:latest
+
 ./build_image.sh crawler dev
 ./build_image.sh rescore dev
 ./build_image.sh web dev
