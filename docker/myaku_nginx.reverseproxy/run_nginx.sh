b4cef37a0cc95fb50b2f7409c93117f1b3e5f2e2
FriedRice <n.mclaughlin21@gmail.com>
2019-09-27 15:27:38 -0400

    Updated nginx image runner script to not create a new dummy cert for dev every time it runs

--- docker/myaku_nginx.reverseproxy/run_nginx.sh
+++ docker/myaku_nginx.reverseproxy/run_nginx.sh
@@ -17,45 +17,56 @@ envsubst < $NGINX_RUN_FILES_DIR/nginx_template.conf > \
 
 cert_dir="/etc/letsencrypt/live/$MYAKUWEB_DOMAIN"
 mkdir -p $cert_dir
-if [ "$USE_PROD_CERT" != "1" ]; then
-    # Create dummy cert to use for dev
-    openssl req -x509 -nodes -newkey rsa:4096 -days 365 \
-        -keyout "$cert_dir/privkey.pem" \
-        -out "$cert_dir/fullchain.pem" \
-        -subj "/CN=localhost"
+if [ ! -f "$cert_dir/privkey.pem" ]; then
+    # No cert is available, so we must create a new one
 
-# No cert is available, so get one with certbot
-elif [ ! -f "$cert_dir/privkey.pem" ]; then
-    # Create dummy cert so that nginx will start
-    openssl req -x509 -nodes -newkey rsa:4096 -days 365 \
-        -keyout "$cert_dir/privkey.pem" \
-        -out "$cert_dir/fullchain.pem" \
-        -subj "/CN=localhost"
+    if [ "$USE_PROD_CERT" == "1" ]; then
+        # A cert for prod is needed, so get one with certbot
+        echo "Getting a prod cert using certbot"
 
-    # Start nginx as a daemon so that certbot can reach it
-    nginx
+        # Create dummy cert first so that nginx will start
+        openssl req -x509 -nodes -newkey rsa:4096 -days 365 \
+            -keyout "$cert_dir/privkey.pem" \
+            -out "$cert_dir/fullchain.pem" \
+            -subj "/CN=localhost"
 
-    # Delete dummy certificate and any other certificate remnants
-    rm -Rf /etc/letsencrypt/live
-    rm -Rf /etc/letsencrypt/archive
-    rm -Rf /etc/letsencrypt/renewal
+        # Start nginx as a daemon so that certbot can reach it
+        nginx
 
-    # Get real cert from certbot
-    certbot certonly --webroot -w "$CERTBOT_WEB_ROOT" \
-        -m $CERTBOT_EMAIL -d $MYAKUWEB_DOMAIN \
-        --rsa-key-size 4096 --agree-tos --non-interactive
-    if [ $? -ne 0 ]; then
-        echo "Certbot failed"
-        exit 1
+        # Delete dummy certificate and any other certificate remnants
+        rm -Rf /etc/letsencrypt/live
+        rm -Rf /etc/letsencrypt/archive
+        rm -Rf /etc/letsencrypt/renewal
+
+        # Get a prod cert from certbot
+        certbot certonly --webroot -w "$CERTBOT_WEB_ROOT" \
+            -m $CERTBOT_EMAIL -d $MYAKUWEB_DOMAIN \
+            --rsa-key-size 4096 --agree-tos --non-interactive
+        if [ $? -ne 0 ]; then
+            echo "Certbot failed"
+            exit 1
+        fi
+        echo "Certbot successfully acquired a cert"
+
+        # Stop nginx so that we can start it in the foreground
+        nginx -s stop
+
+    else
+        # Create dummy cert to use for dev
+        echo "Creating a dummy cert for dev use"
+        openssl req -x509 -nodes -newkey rsa:4096 -days 365 \
+            -keyout "$cert_dir/privkey.pem" \
+            -out "$cert_dir/fullchain.pem" \
+            -subj "/CN=localhost"
     fi
-    echo "Certbot successfully acquired a cert"
 
-    # Stop nginx so that we can start it in the foreground
-    nginx -s stop
-
-# Cert is available, so try renewing it
 else
-    certbot renew
+    echo "Using existing cert"
+
+    if [ "$USE_PROD_CERT" == "1" ]; then
+        # Prod cert is available, so try renewing it
+        certbot renew
+    fi
 fi
 
 # Errors can happen if nginx starts at the same time uwsgi is starting in the
