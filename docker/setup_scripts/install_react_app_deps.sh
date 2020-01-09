f315b1e16d834d152b1eb0aaf5a11b1b85d90578
FriedRice <n.mclaughlin21@gmail.com>
2020-01-08 19:50:33 -0500

    Fix react deps install script failing during travis setup

--- docker/setup_scripts/install_react_app_deps.sh
+++ docker/setup_scripts/install_react_app_deps.sh
@@ -6,7 +6,7 @@ set -ev
 
 sudo apt-get install -y apt-transport-https ca-certificates curl gnupg-agent
 curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
-sudo apt-get install -y nodejs=13.1.0-1nodesource1
+sudo apt-get install -y nodejs
 
 cd "$(dirname "${BASH_SOURCE[0]}")/../../myakuweb-clientapp"
 npm install
